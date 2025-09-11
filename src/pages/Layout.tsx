9d06165918e9bed29e5797cb13c44b6051d0e1de
silverstar9482 <silverstar9482@gmail.com>
2025-09-11 09:17:43 +0900

    fit/ footer 파일 위치변경

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header.tsx';
 import Main from './Main.tsx';
+import Footer from '../components/footer/Footer';
 
 const Layout = () => {
     return (
         <>
             <Header />
             <Main />
+            <Footer />
         </>
     );
 };
