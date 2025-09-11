fd21215abd8c4407a2a0516bfecb36d8c306b2e5
sc.lee <sclee9040@gmail.com>
2025-09-11 17:07:58 +0900

    feat : Nav 이미지 활성화 작업

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,12 @@
 import Header from '../components/header/Header.tsx';
 import Main from './Main.tsx';
 import Footer from '../components/footer/Footer';
-
+import Nav from '../components/nav/Nav';
 const Layout = () => {
     return (
         <>
             <Header />
+            <Nav />
             <Main />
             <Footer />
         </>
