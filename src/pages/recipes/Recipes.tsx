af80e09e664f568aa93a849f768b07adbb822c38
gimeast <slasnrndu@gmail.com>
2025-09-16 00:40:09 +0900

    feature: 레시피 컴포넌트 카테고리, 정렬, 레시피작성 UI 완료

--- src/pages/recipes/Recipes.tsx
+++ src/pages/recipes/Recipes.tsx
@@ -34,7 +34,7 @@ const Recipes = ({ query }: { query?: string }) => {
 
     const handleModalConfirm = () => {
         if (modalType === 'LOGIN') {
-            navigate('/login'); // 로그인 페이지로 이동
+            navigate('/login');
         }
         closeModal();
     };
@@ -53,66 +53,88 @@ const Recipes = ({ query }: { query?: string }) => {
         <div className={styles['recipe']}>
             <section className={styles['recipe__category']}>
                 <h2 className='sr-only'>카테고리</h2>
-                <ul>
-                    <li>
-                        <button type='button' onClick={() => updateCategory('')}>
-                            전체
-                        </button>
-                    </li>
-                    {categories.map(data => (
-                        <li key={data.id}>
-                            <button type='button' onClick={() => updateCategory(data.id)}>
-                                {data.name}
+                <div className={styles['recipe__category-border']}>
+                    <ul>
+                        <li>
+                            <button
+                                className={styles['recipe__category-button']}
+                                type='button'
+                                onClick={() => updateCategory('')}
+                            >
+                                전체
                             </button>
                         </li>
-                    ))}
-                </ul>
+                        {categories.map(data => (
+                            <li key={data.id}>
+                                <button
+                                    className={styles['recipe__category-button']}
+                                    type='button'
+                                    onClick={() => updateCategory(data.id)}
+                                >
+                                    {data.name}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </section>
 
-            <div className={styles['recipe__search']}>
-                <label htmlFor='search'>검색어</label>
-                <input id='search' defaultValue={query} onChange={e => updateSearchTerm(e.target.value)} />
-            </div>
+            <section className={styles['recipe__contents-box']}>
+                <div className={styles['recipe__search']}>
+                    <label htmlFor='search'>검색어</label>
+                    <input id='search' defaultValue={query} onChange={e => updateSearchTerm(e.target.value)} />
+                </div>
 
-            <div className={styles['recipe__button-box']}>
-                <ul className={styles['recipe__order']}>
-                    <li>
-                        <button type='button' onClick={() => handleFilter('recently')}>
-                            최신순
+                <div className={styles['recipe__button-box']}>
+                    <ul className={styles['recipe__order']}>
+                        <li>
+                            <button type='button' onClick={() => handleFilter('recently')}>
+                                최신순
+                            </button>
+                        </li>
+                        <li>
+                            <button type='button' onClick={() => handleFilter('recommended')}>
+                                추천순
+                            </button>
+                        </li>
+                        <li>
+                            <button type='button' onClick={() => handleFilter('popular')}>
+                                인기순
+                            </button>
+                        </li>
+                    </ul>
+
+                    {user ? (
+                        <NavLink className={styles['recipe__write-btn']} to='/recipes/form'>
+                            레시피 작성
+                        </NavLink>
+                    ) : (
+                        <button
+                            className={styles['recipe__write-btn']}
+                            type='button'
+                            onClick={() => openModal('LOGIN')}
+                        >
+                            레시피 작성
                         </button>
-                    </li>
-                    <li>
-                        <button type='button' onClick={() => handleFilter('recommended')}>
-                            추천순
-                        </button>
-                    </li>
-                    <li>
-                        <button type='button' onClick={() => handleFilter('popular')}>
-                            인기순
-                        </button>
-                    </li>
-                </ul>
+                    )}
+                </div>
 
-                {user ? (
-                    <NavLink to='/recipes/form'>레시피 등록하기</NavLink>
-                ) : (
-                    <button type='button' onClick={() => openModal('LOGIN')}>
-                        레시피 등록하기
-                    </button>
-                )}
-            </div>
+                <section className={styles['recipe__list-box']}>
+                    <h2 className='sr-only'>레시피 목록</h2>
 
-            <section className={styles['recipe__list']}>
-                <h2 className='sr-only'>레시피 목록</h2>
-
-                {searchList.map(item => (
-                    <PostItem
-                        key={item.id}
-                        post={item}
-                        type='recipe'
-                        onClick={postId => navigate(`/recipes/${postId}`)}
-                    />
-                ))}
+                    <ul className='recipe__list'>
+                        {searchList.map(item => (
+                            <li>
+                                <PostItem
+                                    key={item.id}
+                                    post={item}
+                                    type='recipe'
+                                    onClick={postId => navigate(`/recipes/${postId}`)}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </section>
             </section>
 
             <Modal isOpen={isOpen} type={modalType} onClose={closeModal} onConfirm={handleModalConfirm} />
