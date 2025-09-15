2a6c41406f4ad075c31565c99a8cbac0675f5af5
gimeast <slasnrndu@gmail.com>
2025-09-15 11:19:48 +0900

    refactor: 버튼 컴포넌트 위치 이동

--- src/components/button/KakaoLoginButton.tsx
+++ src/components/button/KakaoLoginButton.tsx
@@ -1,4 +1,4 @@
-import supabase from '../services/supabaseClient.ts';
+import supabase from '../../services/supabaseClient.ts';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // 혹은 Next.js useRouter
 
